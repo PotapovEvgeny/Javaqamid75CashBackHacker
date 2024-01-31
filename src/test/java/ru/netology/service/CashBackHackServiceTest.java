f9df19d416f20aa85cd4ad79b84b4d54bf501fe2
Evgen7985 <Evgen7985@yandex.ru>
2024-01-31 15:15:58 +0500

    First commit

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+    CashBackHackService service = new CashBackHackService();
+
+    @Test
+    public void shouldAmountLess1000() {
+        int expected = 100;
+        int actual = service.remain(900);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountUnder1000() {
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountIs1000() {
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+}
